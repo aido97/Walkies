b0baea9b86dcc19f39478769b4ef5deb17464f66
James-Curran <x15758929@student.ncirl.ie>
2017-11-14 10:08:40 +0000

    Removed useless php

--- index.php
+++ index.php
@@ -254,36 +254,6 @@ print "<button type=\"button\" class=\"btn btn-primary becWalkerBtn\">Become a W
     ?>
     
 
-<<<<<<< HEAD
-                            <?php
-                            /*
-                            * If login url is there then display login button
-                            * else print the retieved data
-                            
-                            if (isset($authUrl)) {
-                                echo "<a class='login' href='" . $authUrl . "'><img src='gplus-lib/signin_button.png' height='50px'/></a>";
-                            } else {
-                                print "ID: {$id} <br>";
-                                print "Name: {$name} <br>";
-                                print "Email: {$email } <br>";
-                                print "Image : {$profile_image_url} <br>";
-                                print "Cover  :{$cover_image_url} <br>";
-                                print "Url: {$profile_url} <br><br>";
-                                echo "<a class='logout' href='?logout'><button>Logout</button></a>";
-                            }
-                            
-                            if($_POST){
-
-                            $mysqli = new mysqli("localhost", "shaner125", "", "db/walkies_TP.sql");
-                            $mysqli->query("INSERT INTO table users (user_email, first_name) VALUES ('$email','$name');
-                           
-
-                            }
-                            
-                            */?>
-                            
-=======
->>>>>>> 78fa6088359d402c442901fe06a7a1b4d4a628ef
                             
 	<!-- Footer -->
 	<footer class="footer">
