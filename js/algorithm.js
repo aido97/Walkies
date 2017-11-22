498ccb5166c53275327cce81c2dfcf694b633f39
root <x15758929@student.ncirl.ie>
2017-11-22 20:49:20 +0000

    TSP fully finished, optimized and complete

--- js/algorithm.js
+++ js/algorithm.js
@@ -1,50 +1,48 @@
-
-//Calculates the fitness for every population dustrubution
-function getFitness() {
-var currentRecord = 999999;    
-   for(var i = 0; i < population.length; i++){
+//Calculates the desirability for every population dustrubution
+function getDesirability() {   
+for(var i = 0; i < populationDensity; i++){
     var distance = findTotalDistance(dogDist, population[i]);
     //Sets record to the shortest distance and bestOrder to the population that spwaned that distance
     if(distance < record){
         record = distance;
         bestOrder = population[i];
     }
-    if (distance < currentRecord) {
-        currentRecord = distance;
-        currentBest = population[i];
-      }
-    //Dividing by one makes it so that larger numbers become smaller, and smaller ones become larger. This is because we want to give the smallest distance the largest fitness
-    //+1 is there to stop bugs in case distance was ever 0, however that's very unlikely due to two points not being on top of each other. Better safe than sorry though!
-    fitness[i] = 1 / (Math.pow(distance, 8) + 1);
-    
-    console.log(record);
-    
-}
+    //Multiplies the distance to the power 12 to increase the difference between good distances and bad ones, making good ones a lot 
+    //more likely as they will take up a much bigger percentage of the overally desirability, which is used to determine its probability.
+    //Dividing by one inverts the number (because we want the smallest distance to be the most likely and therefore highest number)
+    //Adding one is there incase the distance is ever 0 and we get a nasty error, not likely but better safe than sorry!
+    desirability[i] = 1 / (Math.pow(distance, 12) + 1); 
+    }
 }
 
-function normalizeFitness() {
+//Turns the desirability values into the percentage of the total desirability 
+function normalizeDesirability() {
+    //Adds up all of the desirability values
     var sum = 0;
-    for(var i = 0; i < fitness.length; i++){
-        sum = sum + fitness[i];
+    for(var i = 0; i < populationDensity; i++){
+        sum = sum + desirability[i];
     }
-    for(var i = 0; i < fitness.length; i++){
-        fitness[i] = fitness[i] / sum;
+    //Transforms each value into it's relevant percentage of the total desirability
+    for(var i = 0; i < populationDensity; i++){
+        desirability[i] = desirability[i] / sum;
     }
 }
 
+//Creates a cross over of two arrays of orders
 function crossOver(orderA, orderB){
-
+    //Generates a random start and end point, ensuring that the end is after the start. .slice makes it so we can go passed the 
+    //last index of the order without getting an error.
     var start = Math.floor(Math.random() * orderA.length);
     var end = Math.floor((Math.random() * orderA.length) + (start + 1));
     var newOrder = orderA.slice(start, end);
-
-    //var left = dogDist.length -newOrder.length;
+    //Add all the elements in from B as long as they're not already in A
     for (var i = 0; i < orderB.length; i++){
         var dog = orderB[i];
         if(!newOrder.includes(dog)) {
             newOrder.push(dog);
         }
     }
+    //Return the newly created order
     return newOrder;
 }
 
@@ -52,35 +50,41 @@ function crossOver(orderA, orderB){
 function nextGeneration(){
     var newPopulation = [];
     for(var i = 0; i < population.length; i++){
-        var orderA = pickOne(population, fitness); 
-        var orderB = pickOne(population, fitness);   
-        var order = crossOver(orderA, orderB);      
-        mutate(order, 0.01);
+        //Gets two of the best populations and then crosses them over 
+        var orderA = pickOne(population, desirability); 
+        var orderB = pickOne(population, desirability);   
+        var order = crossOver(orderA, orderB);
+        //Mutate at a rate of 8.5%      
+        mutate(order, 0.085);
         newPopulation[i] = order; 
     }
-    population = newPopulation;
-    
+    population = newPopulation;   
 }
 
-//Picks a number from 0.0 - 1 that represents 1-100% and chooses according to the corresponding fitness range 
-function pickOne(list, fitness) {
+//Picks a random number from 0.0 - 1. Subtracts the desirability score (now percentage of total desirability remember) from the generated
+//number. This results in the scores with the highest desirability score having the highest probability to trigger the switch from r > 0
+//Each score has a chance of triggering r to become negative directly relational to it's desirability score.
+function pickOne(list, desirability) {
     var index = 0;
     var r = Math.random(1);
+    //While r is still a positive number, keep subtracting the next desirability index until it becomes negative
     while (r > 0) {
-        r = r - fitness[index];
+        r = r - desirability[index];
         index++;
       }
+      //Compensates for the final unnecessary index increment, it was not this index that tipped us over the edge!
       index--;
-    
-    r = r - fitness[index];
+    //Return the corresponding order from the successful desirability value
     return list[index].slice();
   }
   
-  //Mutates the given order
+  //Mutates a given order by a given % mutation rate
   function mutate(order, mutationRate) {
     for (var i = 0; i < totalDogs; i++) {
+        //If a randomly generated number between 0.0 and 1 is less than the mutation rate i.e. in mutationRate % of cases this happens
         if (Math.random(1) < mutationRate) {
-          var indexA = Math.floor(Math.random(order.length));
+        //Swaps two random elements in the array
+          var indexA = Math.floor(Math.random() * totalDogs);
           var indexB = (indexA + 1) % totalDogs;
           swap(order, indexA, indexB);
         }
